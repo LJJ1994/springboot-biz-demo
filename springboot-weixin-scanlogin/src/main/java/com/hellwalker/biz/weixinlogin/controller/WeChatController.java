e66a661922f5559e14f31d0ff683ae4769fa6a51
LJJ <optionsci111@gmail.com>
2024-02-19 19:58:24 +0800

    微信移动端登录

--- springboot-weixin-scanlogin/src/main/java/com/hellwalker/biz/weixinlogin/controller/WeChatController.java
+++ springboot-weixin-scanlogin/src/main/java/com/hellwalker/biz/weixinlogin/controller/WeChatController.java
@@ -5,6 +5,7 @@ import com.hellwalker.common.exception.BizException;
 import com.hellwalker.common.result.CommonResult;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
 import me.chanjar.weixin.common.bean.oauth2.WxOAuth2AccessToken;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.common.service.WxOAuth2Service;
@@ -34,30 +35,8 @@ public class WeChatController {
     @Autowired
     private WechatAccountConfig wechatAccountConfig;
 
-    /*
-     * @param signature 微信加密签名，signature结合了开发者填写的 token 参数和请求中的 timestamp 参数、nonce参数。
-     * @param timestamp 时间戳
-     * @param nonce     这是个随机数
-     * @param echostr   随机字符串，验证成功后原样返回
-     */
-    @GetMapping
-    public void get(@RequestParam(required = false) String signature,
-                    @RequestParam(required = false) String timestamp,
-                    @RequestParam(required = false) String nonce,
-                    @RequestParam(required = false) String echostr,
-                    HttpServletResponse response) throws IOException {
-        if (!wxMpService.checkSignature(timestamp, nonce, signature)) {
-            log.warn("接收到了未通过校验的微信消息，这可能是token配置错了，或是接收了非微信官方的请求");
-            return;
-        }
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(echostr);
-        response.getWriter().flush();
-        response.getWriter().close();
-    }
-
     /**
-     * 引导用户访问这个链接：
+     * 【PC端】引导用户访问这个链接：
      * 1、这个链接是网页端，会生成二维码。
      * 2、用户使用微信扫码，进行授权。
      * @return
@@ -75,7 +54,7 @@ public class WeChatController {
     }
 
     /**
-     * 用户授权同意后回调的地址，从请求参数中获取code，state填充returnUrl
+     * PC端登录，用户授权同意后回调的地址，从请求参数中获取code，state填充returnUrl
      * @param code 临时码
      * @param state returnUrl
      * @return
@@ -83,22 +62,23 @@ public class WeChatController {
     @GetMapping("/qr_login/callback")
     public String qrUserInfo(@RequestParam("code") String code,
                              @RequestParam("state") String state) {
-        WxOAuth2AccessToken wxMpOAuth2AccessToken = new WxOAuth2AccessToken();
+        //1. 通过code获取access_token
+        WxOAuth2AccessToken wxOAuth2AccessToken = new WxOAuth2AccessToken();
         try {
-            //通过code获取access_token
-            wxMpOAuth2AccessToken = wxOAuth2Service.getAccessToken(code);
+
+            wxOAuth2AccessToken = wxOAuth2Service.getAccessToken(code);
         } catch (WxErrorException e) {
             log.error("微信网页授权 【异常】 ---> " + e.getMessage());
             return "forward:/api/error";
         }
 
-        log.info("微信登录 token info：" + wxMpOAuth2AccessToken);
+        log.info("微信登录 token info：" + wxOAuth2AccessToken);
 
         //从token中获取openid
-        String openId = wxMpOAuth2AccessToken.getOpenId();
+        String openId = wxOAuth2AccessToken.getOpenId();
 
         // TODO: 进行业务逻辑操作，
-        //  1. 查询数据库，查询third_oauth表，判断是否存在用户
+        //  1. 查询数据库，查询third_oauth表和业务系统用户表，判断是否存在用户
         //  2. 如果不存在，重定向到客户端，提示用户输入手机号、验证码进行验证；否则不给予注册登录。
         //  3. 如果已经存在，更新数据库token，重定向到登录成功的页面；或者执行returnUrl；
         //  4. 提示：可以在微信的url中的state填充returnUrl
@@ -108,7 +88,68 @@ public class WeChatController {
             return "redirect:" + state;
         }
 
+        // 2. 根据access token获取user info
+        WxOAuth2UserInfo oAuth2UserInfo = new WxOAuth2UserInfo();
+        try {
+            oAuth2UserInfo = wxOAuth2Service.getUserInfo(wxOAuth2AccessToken, "cn");
+        } catch (WxErrorException e) {
+            log.error("【移动微信登录】获取 微信用户信息异常：" + e.getMessage());
+        }
+        String nickname = oAuth2UserInfo.getNickname();
+        String headImgUrl = oAuth2UserInfo.getHeadImgUrl();
+
+
+        // TODO: 业务逻辑操作
+        //    查询数据库，更新或创建业务系统用户表信息
+
         // 默认回到主页
         return "index";
+    }
+
+    /**
+     * 【移动端微信登录】请求，根据票据code获取access_token和user info。
+     * 【该接口调用的时机】：移动端在客户端侧调用微信APP授权，用户成功授权后，移动端在微信提供的客户端工具的回调方法中，
+     * 返回临时票据code，移动端应使用http client 使用get请求，调用该接口。参考：https://blog.51cto.com/u_15329836/3387031
+     * @param code
+     * @return
+     */
+    @GetMapping("/sns/mobile/access_token")
+    @ResponseBody
+    public CommonResult mobileQueryToken(@RequestParam("code") String code) {
+        // 1. 获取微信access token
+        WxOAuth2AccessToken wxOAuth2AccessToken = new WxOAuth2AccessToken();
+        try {
+            wxOAuth2AccessToken = wxOAuth2Service.getAccessToken(code);
+        } catch (WxErrorException e) {
+            log.error("【移动微信登录】获取access token异常：" + e.getMessage());
+            return CommonResult.failed(e.getMessage());
+        }
+        String openId = wxOAuth2AccessToken.getOpenId();
+        String accessToken = wxOAuth2AccessToken.getAccessToken();
+        String refreshToken = wxOAuth2AccessToken.getRefreshToken();
+        int expiresIn = wxOAuth2AccessToken.getExpiresIn();
+
+        // TODO: 进行业务逻辑操作，
+        //  1. 查询数据库，查询third_oauth表和业务系统用户表，判断是否存在用户
+        //  2. 如果不存在，重定向到客户端，提示用户输入手机号、验证码进行验证；否则不给予注册登录。
+        //  3. 如果已经存在，更新数据库token.
+        //  4. 生成登录jwt token，写入header，返回成功消息
+
+        // 2. 根据access token获取user info
+        WxOAuth2UserInfo oAuth2UserInfo = new WxOAuth2UserInfo();
+        try {
+            oAuth2UserInfo = wxOAuth2Service.getUserInfo(wxOAuth2AccessToken, "cn");
+        } catch (WxErrorException e) {
+            log.error("【移动微信登录】获取 微信用户信息异常：" + e.getMessage());
+        }
+        String nickname = oAuth2UserInfo.getNickname();
+        String headImgUrl = oAuth2UserInfo.getHeadImgUrl();
+
+
+        // TODO: 业务逻辑操作
+        //    查询数据库，更新或创建业务系统用户表信息
+
+
+        return CommonResult.success(openId);
     }
 }
